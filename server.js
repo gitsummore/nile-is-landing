7a7c8d37dda13e8615179e3b10a1bec47b98a76a
Kevin Qiu <kami.kevinq@gmail.com>
2017-05-16 09:16:55 -0700

    initial commit

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000');
 });
 
-const nileServer = require('nile.js/nileServer')(server);
+// const nileServer = require('nile.js/nileServer')(server);
 
 app.use(express.static(__dirname));
-app.use('/', nileServer);
+// app.use('/', nileServer);
 
 
 
