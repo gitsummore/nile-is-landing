cb50a368741e260e57b048edb37b0f904d1034b9
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-05-16 11:53:05 -0700

    configuring for https

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 
 dotenv.load();
 
+// app.use(requireHTTPS);
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000');
 });
@@ -13,5 +15,11 @@ const server = app.listen(process.env.PORT || 3000, () => {
 app.use(express.static(__dirname));
 // app.use('/', nileServer);
 
-
+// function requireHTTPS(req, res, next) {
+//    if (!req.secure) {
+//        //FYI this should work for local development as well
+//        return res.redirect('https://' + req.get('host') + req.url);
+//    }
+//    next();
+// }
 
