8533d441f8616749ae41819956602c2335f61f10
Derek Miranda <derek.miranda.1992@gmail.com>
2017-05-17 11:20:37 -0700

    One broadcaster limit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 
 dotenv.load();
 
@@ -10,8 +11,56 @@ const server = app.listen(process.env.PORT || 3000, () => {
 
 const nileServer = require('./nileServer')(server);
 
+// stack of stored cookies
+const cookies = {};
+
+app.use(cookieParser());
+
+app.get('/broadcaster.html', (req, res) => {
+  return res.redirect('/broadcaster');
+});
+
+app.get('/broadcaster', (req, res) => {
+  // if first client to request this page, allow them to broadcast
+  if (Object.keys(cookies).length === 0) {
+    // generate arbitrary cookie from current milliseconds
+    const cookie = Date.now();
+
+    // send cookie
+    res.cookie('broadcasterId', cookie);
+
+    // store cookie
+    cookies[cookie] = true;
+
+    res
+      // prevent storing page
+      .set('Cache-Control', 'no-store')
+      .sendFile(`${__dirname}/broadcaster.html`);
+  } else {
+    res.send('Someone\'s currently broadcasting. Sorry mate!');
+  }
+});
+
+// listen for Broadcaster page closes
+// remove request's cookie from cookie store
+app.get('/broadcaster-disconnect', (req, res) => {
+  const cookieToRemove = req.cookies.broadcasterId;
+
+  let message;
+  if (cookies[cookieToRemove]) {
+    // remove disconnecting client's cookie
+    delete cookies[cookieToRemove];
+    message = 'Cookie cleared';
+  } else {
+    message = 'No cookie to clear';
+  }
+  return res.status(200).send(message);
+});
+
 app.use(express.static(__dirname));
 app.use('/', nileServer);
 
-
+function handleError(err) {
+  throw err;
+}
 
