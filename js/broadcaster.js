d65027c34e1dede19aaaaae335c7a79a14f41ca3
Derek Miranda <derek.miranda.1992@gmail.com>
2017-05-19 11:28:28 -0700

    Removed demo links and disabled single broadcaster restriction

--- js/broadcaster.js
+++ js/broadcaster.js
@@ -1,27 +1,3 @@
 const broadcaster = new Broadcaster(6000, 'video', 'play', 'stop');
 
 document.getElementById('broadcaster').setAttribute("class", "embed-responsive-item");
-
-// Limit user's session if inactive
-var pageTimer;
-
-var playBtn = document.getElementById('play');
-var stopBtn = document.getElementById('stop');
-
-var inactiveTime = 5000;
-var inactiveFunc = () => {
-  window.close();
-};
-
-playBtn.addEventListener('click', (event) => {
-  // create timer
-  pageTimer = window.setTimeout(inactiveFunc, inactiveTime);
-});
-
-// send message to server that disconnecting
-window.onbeforeunload = function (event) {
-  // create get request to tell about disconnect
-  const req = new XMLHttpRequest();
-  req.open('GET', '/broadcaster-disconnect', false);
-  req.send();
-};
