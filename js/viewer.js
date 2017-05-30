305ae253f8430f2955420b25b59102d410b69af1
Kevin Qiu <kami.kevinq@gmail.com>
2017-05-30 07:16:05 -0700

    added download and upload bar to viewer page

--- js/viewer.js
+++ js/viewer.js
@@ -41,7 +41,6 @@ var myBarChart = new Chart(ctx, {
   options: {
     scales: {
       yAxes: [{
-        // type: 'logarithmic',
         ticks: {
           beginAtZero: true
         },
@@ -50,6 +49,33 @@ var myBarChart = new Chart(ctx, {
           labelString: 'Megabytes (MB)'
         }
       }]
+    },
+    "hover": {
+      "animationDuration": 0
+    }, "animation": {
+      "duration": 1,
+      "onComplete": function () {
+        var chartInstance = this.chart,
+          ctx = chartInstance.ctx;
+
+        ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'bottom';
+
+        this.data.datasets.forEach(function (dataset, i) {
+          var meta = chartInstance.controller.getDatasetMeta(i);
+          meta.data.forEach(function (bar, index) {
+            var data = dataset.data[index].toFixed(2);
+            ctx.fillText(data, bar._model.x, bar._model.y - 5);
+          });
+        });
+      }
+    },
+    legend: {
+      "display": false
+    },
+    tooltips: {
+      "enabled": false
     }
   }
 });
@@ -60,16 +86,11 @@ let test = true;
 let up;
 
 window.setInterval(() => {
-  if (test) {
-    up = Math.random() + 6;
-  } else {
-    up = Math.random() + 5;
-  }
   let currTotals = viewer.returnTotals();
 
-  uploaded += prettyBytes(currTotals.downloaded / up);
+  uploaded += toMB(currTotals.uploaded / 4);
 
-  downloaded += prettyBytes(currTotals.downloaded / 4);
+  downloaded += toMB(currTotals.downloaded / 4);
   // console.log('currTotals', currTotals);
   data.datasets[0].data[0] = downloaded;
   data.datasets[1].data[0] = uploaded;
@@ -80,6 +101,6 @@ window.setInterval(() => {
 
 
 // convert bytes to Mega Bytes
-function prettyBytes(num) {
+function toMB(num) {
   return num / 1000000
 }
